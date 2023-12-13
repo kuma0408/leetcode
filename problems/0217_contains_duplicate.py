bee4bc3937d20dd582ba6431834d43f36652e6da
kuma <gigglingirl1225@gmail.com>
2023-12-13 21:10:29 +0900

    217 contains duplicate

---
+++ problems/0217_contains_duplicate.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/contains-duplicate/submissions/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            s.add(num)
+        return False
