5f1839fc88ea198112592458e0f12a64847681a5
kuma <gigglingirl1225@gmail.com>
2023-12-19 23:01:02 +0900

    15. 3Sum

--- problems/0015_3Sum.py
+++ problems/0015_3Sum.py
@@ -2,16 +2,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ret = []
-        s = set(nums)
-        while len(s) > 0:
-            target = s.pop()
-            skip_set = set()
-            for element in s:
-                if element in skip_set:
-                    continue
+        nums = sorted(nums)
 
-                if -(element+target) in s:
-                    ret.append([target, element, -(element+target)])
-                    skip_set.add(-(element+target))
+        for i, num in enumerate(nums):
+            if num > 0:
+                break
+
+            # To avoid dupulicated answer, skip the same value as the previous one.
+            if i > 0 and (num == nums[i-1]):
+                continue
+            l = i+1
+            r = len(nums)-1
+
+            while l < r:
+                tmp_sum = num + nums[l] + nums[r]
+
+                if tmp_sum == 0:
+                    ret.append([num, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+                elif tmp_sum >= 0:
+                    r -= 1
+                else:
+                    l += 1
 
         return ret
