f213cae77f38be37f7b2922e447cbafd11457f0e
kuma <gigglingirl1225@gmail.com>
2023-12-23 10:20:34 +0900

    155. Min Stack途中まで

---
+++ problems/0155_MinStack.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.li = []
+        self.min_li = []
+        self.min = 2 ** 31
+
+    def push(self, val: int) -> None:
+        self.li.append(val)
+        if self.min >= val:
+            self.min_li.append(val)
+            self.min = val
+        else:
+            self.min_li.append(self.min)
+
+    def pop(self) -> None:
+        self.li.pop()
+        self.min_li.pop()
+        if len(self.li) == 0:
+            self.min = 2 ** 31
+
+    def top(self) -> int:
+        return self.li[-1]
+
+    def getMin(self) -> int:
+        return self.min_li[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
