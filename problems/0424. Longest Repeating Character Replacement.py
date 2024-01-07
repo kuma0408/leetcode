55aaa1070e75bd0d27ba62cd7567b6006990bd19
kuma <gigglingirl1225@gmail.com>
2024-01-07 23:44:32 +0900

    424. Longest Repeating Character Replacement

--- problems/0424. Longest Repeating Character Replacement.py
+++ problems/0424. Longest Repeating Character Replacement.py
@@ -1,3 +1,19 @@
+from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
+        d = defaultdict(int)
+        l = 0
+        ans = 0
+
+        for i in range(len(s)):
+            d[s[i]] += 1
+
+            freq_char_count = max(d.values())
+
+            if (i - l + 1) - freq_char_count <= k:
+                ans = max(ans, i-l+1)
+            else:
+                d[s[l]] -= 1
+                l += 1
+
+        return ans
